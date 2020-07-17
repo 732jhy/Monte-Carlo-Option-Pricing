2d355f9049fb90ea09e7d3bab5464fb5cb275e51
Justin Yu <732jhy@gmail.com>
2020-07-17 04:01:05 -0400

    Gamma-based Control Variate added

--- European Options.py
+++ European Options.py
@@ -4,14 +4,21 @@ Created on Fri Jul  3 16:12:30 2020
 
 @author: Justin Yu, M.S. Financial Engineering, Stevens Institute of Technology
 
-Monte Carlo simulations for European option pricing (simple, AV, and CV)
+Monte Carlo simulations for European option pricing
+Pricing algorithms include:
+    - Simple
+    - Antithetic Variate only
+    - Delta-based Control Variate only
+    - Delta- and Gamma-based Control Variates
+    - Delta-based Control Variates and Antithetic Variates
 """
+
 import numpy as np
 import scipy.stats as si
 
 def monte_carlo(S,K,T,r,q,sigma,CallPut,n,m):
     '''
-    Monte Carlo option pricing algorithm for European Calls and Puts with Euler discretization
+    Simple Monte Carlo option pricing algorithm for European Calls and Puts with Euler discretization
     
     Args:
         S - Initial Stock Price         sigma - Volatility
@@ -24,7 +31,7 @@ def monte_carlo(S,K,T,r,q,sigma,CallPut,n,m):
     associated with the simulation
     '''
     
-    def GBM_sim(mu,sigma,T,S,N, M):
+    def path_sim(mu,sigma,T,S,N, M):
         '''Simulates M paths of Geometric Brownian Motion with N time steps'''
         sims = np.zeros(M) 
         dt = T/N
@@ -36,7 +43,7 @@ def monte_carlo(S,K,T,r,q,sigma,CallPut,n,m):
         St = S*np.exp((mu-0.5*sigma**2)*T + sigma*sims)     
         return St
    
-    paths = GBM_sim(r-q,sigma,T,S,n,m)
+    paths = path_sim(r-q,sigma,T,S,n,m)
     
     if CallPut == 'Call':
         paths = paths - K
@@ -68,7 +75,7 @@ def monte_carlo_AV(S,K,T,r,q,sigma,CallPut,n,m):
     Returns the simulated price of a European call or put with standard deviation and standard error
     associated with the simulation
     '''
-    def GBM_sim(mu,sigma,T,S,N, M):
+    def path_sim(mu,sigma,T,S,N, M):
         '''Simulates M paths of Geometric Brownian Motion with N time steps incorporating antithetic variance reduction'''
         sims = np.zeros(M)
         dt = T/N
@@ -81,7 +88,7 @@ def monte_carlo_AV(S,K,T,r,q,sigma,CallPut,n,m):
         Sta = S*np.exp((mu-0.5*sigma**2)*T - sigma*sims)    
         return np.array([St, Sta])
         
-    paths = GBM_sim(r-q,sigma,T,S,n,m)
+    paths = path_sim(r-q,sigma,T,S,n,m)
     
     if CallPut == 'Call':
         paths = paths - K
@@ -99,9 +106,9 @@ def monte_carlo_AV(S,K,T,r,q,sigma,CallPut,n,m):
     return c_hat, sd, se
 
 
-def monte_carlo_CV(S,K,T,r,q,sigma,CallPut,n,m):
+def monte_carlo_deltCV(S,K,T,r,q,sigma,CallPut,n,m):
     '''
-    Monte Carlo European option pricing algorithm with Delta-based control variate with Euler discretization
+    Monte Carlo European option pricing algorithm with Delta-based Control Variate with Euler discretization
     
     Args:
         S - Initial Stock Price         sigma - Volatility
@@ -121,7 +128,7 @@ def monte_carlo_CV(S,K,T,r,q,sigma,CallPut,n,m):
         elif c_p == 'Put':
             return si.norm.cdf(d1, 0.0, 1.0) - 1
         
-    def GBM_sim(mu,sigma,T,S,N, M, cp,K):
+    def path_sim(mu,sigma,T,S,N, M, cp,K):
         '''Simulates M paths of Geometric Brownian Motion with N time steps incorporating antithetic variance reduction'''
         sims = list(np.zeros(M));
         t = np.linspace(0,T,N);        dt = T/N
@@ -135,18 +142,18 @@ def monte_carlo_CV(S,K,T,r,q,sigma,CallPut,n,m):
             cv = np.sum(d*(St[1:N] - (St[0:N-1]*erddt))*np.exp(mu*ttm[1:N]))
             sims[i] = [St[-1], cv]
         return np.array(sims).T
-    beta1 = -1.0 
-    paths = GBM_sim(r-q,sigma,T,S,n,m,CallPut,K)
+
+    paths = path_sim(r-q,sigma,T,S,n,m,CallPut,K)
 
     if CallPut == 'Call':
         paths[0] = paths[0] - K
         paths[0][paths[0] < 0] = 0
-        paths = paths[0] + beta1*paths[1]
+        paths = paths[0] - paths[1]
         
     elif CallPut == 'Put':
         paths[0] = K - paths[0]
         paths[0][paths[0] < 0] = 0
-        paths = paths[0] + beta1*paths[1]
+        paths = paths[0] - paths[1]
     
     c_hat = np.mean(paths)*np.exp(-r*T)    
     sd = np.sqrt(np.sum((paths*np.exp(-r*T) - c_hat)**2)/(m-1))
@@ -155,9 +162,80 @@ def monte_carlo_CV(S,K,T,r,q,sigma,CallPut,n,m):
     return c_hat, sd, se
 
 
-def monte_carlo_AVCV(S,K,T,r,q,sigma,CallPut,n,m):
+def monte_carlo_delt_gamCV(S,K,T,r,q,sigma,CallPut,n,m):
     '''
-    Monte Carlo European option pricing algorithm with Delta-based control variate and Antithetic Variates
+    Monte Carlo European option pricing algorithm with Delta-based and Gamma-based Control Variates with Euler discretization
+    
+    Args:
+        S - Initial Stock Price         sigma - Volatility
+        K - Strike Price                CallPut - Option type specification (Call or Put)
+        T - Time to Maturity            n - number of time steps for each MC path
+        r - Risk-free interest rate     m - number of simulated paths
+        q - Dividend rate
+    
+    Returns the simulated price of a European call or put with standard deviation and standard error
+    associated with the simulation
+    '''
+    def delta(S, K, T, r, sigma, CallPut):
+        '''Black-Scholes delta'''
+        d1 = (np.log(S/K) + (r + 0.5*sigma**2)*T)/(sigma*np.sqrt(T))
+        if CallPut == 'Call':    
+            return si.norm.cdf(d1, 0.0, 1.0)
+        elif CallPut == 'Put':
+            return si.norm.cdf(d1, 0.0, 1.0) - 1
+    
+    def gamma(S, K, T, r, q, sigma):
+        '''Black-Scholes gamma'''
+        d1 = (np.log(S/K) + (r - q + 0.5*sigma**2)*T)/(sigma*np.sqrt(T))
+        return (np.exp(-q*T)/(S*sigma*np.sqrt(T))) * ((2*np.pi)**(-0.5)) * np.exp(-0.5*d1**2)
+    
+    def path_sim(r,q,sigma,T,S,N, M,CallPut,K):
+        '''Simulates paths of Geometric Brownian motion while implementing the control variates'''
+        sims = list(np.zeros(M));
+        t = np.linspace(0,T,N);        dt = T/N
+        ttm = np.flip(t) 
+        erddt = np.exp((r-q)*dt)   
+        egamma = np.exp((2*(r-q) + sigma**2)*dt) - 2*erddt + 1
+
+        for i in range(M):
+            # GBM path:
+            W = [0]+np.random.standard_normal(size=N)
+            W = np.cumsum(W)*np.sqrt(dt)
+            St = S*np.exp((r-q-0.5*sigma**2)*t + sigma*W)
+            
+            # Delta CV:
+            d = delta(St[0:N-1],K,ttm[0:N-1],r-q,sigma,CallPut)                
+            cv_d = np.sum(d*(St[1:N] - (St[0:N-1]*erddt))*np.exp((r-q)*ttm[1:N]))
+            
+            # Gamma CV:
+            g = gamma(St[0:N-1], K, ttm[0:N-1], r,q, sigma)     
+            cv_g = np.sum(g*(((St[1:N] - St[0:N-1])**2) - egamma*St[0:N-1]**2)*np.exp((r-q)*ttm[1:N]))
+            sims[i] = [St[-1], cv_d, cv_g]
+        return np.array(sims).T
+        
+    paths = path_sim(r,q,sigma,T,S,n,m,CallPut,K)
+
+    if CallPut == 'Call':
+        paths[0] = paths[0] - K
+        paths[0][paths[0] < 0] = 0
+        paths = paths[0] - paths[1]- 0.5*paths[2]
+        
+    elif CallPut == 'Put':
+        paths[0] = K - paths[0]
+        paths[0][paths[0] < 0] = 0
+        paths = paths[0] - paths[1] - 0.5*paths[2]
+    
+    c_hat = np.mean(paths)*np.exp(-r*T)    
+    sd = np.sqrt(np.sum((paths*np.exp(-r*T) - c_hat)**2)/(m-1))
+    se = sd/np.sqrt(m)
+      
+    return c_hat, sd, se
+
+
+
+def monte_carlo_deltCV_AV(S,K,T,r,q,sigma,CallPut,n,m):
+    '''
+    Monte Carlo European option pricing algorithm with Delta-based Control Variate and Antithetic Variate
     with Euler discretization.
     
     Args:
